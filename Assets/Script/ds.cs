a2dba34f903e6f7e989fe80094260ce641c9c017
Kakami Souma <nkc.souma.kakami@gmail.com>
2018-11-22 14:17:01 +0900

    あとが画像の読み込みだけ

---
+++ Assets/Script/ds.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ds : MonoBehaviour {
+
+	public Texture2D test;
+	public GameObject r;
+	public GameObject b;
+	public Text text;
+	string ggggg;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+
+		for (int i = 62; i > 0; i--)
+		{
+			for (int j = 0; j < 128; j++)
+			{
+				if (test.GetPixel(j,i).r >= 0.5f)
+				{
+					ggggg += "0";
+				}
+				else
+				{
+					ggggg += "1";
+				}
+			}
+			ggggg += "\n";
+		}
+		
+		text.text = ggggg;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
